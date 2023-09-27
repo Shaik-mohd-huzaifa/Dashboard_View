f5c7637d31e1fa4bc6d7f3c62c82d6ccf85d59dd
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-09-27 12:55:41 +0530

    Add  the env variables to supabase associated onces

--- app/api/supabase.js
+++ app/api/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://gubymwuuroyagpdpgdif.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1Ynltd3V1cm95YWdwZHBnZGlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQyNjE5MjUsImV4cCI6MjAwOTgzNzkyNX0.pKlgHkkI0l3mX2HCi988LyC5mos9k1QV1yLnU-8ssUk"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = SUPABASE_SERVICE_ROLE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
