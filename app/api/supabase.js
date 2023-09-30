bbb4ac3ddeba0afb4a36402128653b542fc386e9
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-09-30 18:12:48 +0530

    Configured the key

--- app/api/supabase.js
+++ app/api/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = SUPABASE_SERVICE_ROLE_KEY;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
