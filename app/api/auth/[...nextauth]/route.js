98e627b1b0b5b117b030b4a89e63eceaae02653a
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-09-30 20:14:27 +0530

    dfdfd

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,16 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google"
 
+
 const authOption = {
     providers:[
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
+        }),
     ],
     secret: process.env.NEXTAUTH_SECRET || 'AIzaSyCB5TdGg39DCc4n0TwmSnLSo5R6ZZzhKQ0' ,
+    callbackUrl: process.env.NEXTAUTH_URL
 }
 
 const handler = NextAuth(authOption);
